1f4467d9331cbfa4f5fbed66320fa5ba7b9fec7f
basharghawi <b.alghawi21@gmail.com>
2024-05-30 20:55:34 +0300

    created service section

---
+++ app/components/service-sec/service-sec.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import "./service-sec.css";
+
+export default function ServiceSec() {
+  return (
+    <section className="service-sec">
+      <div className="row align-items-center">
+        <div className="col-12 col-md-6 col-lg-6">
+          <div className="service-sec__box">
+            <div className="service-sec__box-img">
+              <Image src="/images/service-img2-1.jpg" alt="hashtag" width={1430} height={1080} />
+            </div>
+            <div className="service-sec__box-info">
+              <i>
+                <Image src="/images/service-sec-icon1.png" alt="anal" width={52} height={60} />
+              </i>
+              <h4>Web Analytics</h4>
+              <p>Praesent fermentum elit sed libero egestas ullamcorper. Mauris pharetra nulla id augue auctor</p>
+              <Link href="/services">Read More</Link>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-6">
+          <div className="service-sec__box">
+            <div className="service-sec__box-img">
+              <Image src="/images/service-img2-2.jpg" alt="announce" width={1430} height={1080} />
+            </div>
+            <div className="service-sec__box-info">
+              <i>
+                <Image src="/images/service-sec-icon2.png" alt="anal" width={52} height={60} />
+              </i>
+              <h5>“ Praesent fermentum elit sed libero egestas ullamcorper. Mauris pharetra nulla id augue auctor ”</h5>
+              <span>Jason Statham</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 import Hero from "./components/Hero/hero";
 import About from "./components/about-what/about";
+import ServiceSec from "./components/service-sec/service-sec";
 import Services from "./components/services/services";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Services />
       <About />
+      <ServiceSec />
     </>
   );
 }
