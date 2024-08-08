5d4c25cddb9e0d8c776be71e49acdb1ed6ad3a42
basharghawi <b.alghawi21@gmail.com>
2024-08-08 14:39:54 +0300

    fixed breadcrumb title for type "sub"

--- app/_components/breadcrumbs/breadcrumbs.tsx
+++ app/_components/breadcrumbs/breadcrumbs.tsx
@@ -8,17 +8,18 @@ import { usePathname } from "next/navigation";
 import { FunctionComponent } from "react";
 
 interface Props {
-  type: "landing" | "sub"
+  type: "landing" | "sub",
+  title: string,
+  desc: string
 }
 
-export const Breadcrumbs: FunctionComponent<Props> = ({ type }) => {
+export const Breadcrumbs: FunctionComponent<Props> = ({ type, title, desc }) => {
   const paths = usePathname();
   const pathNames = paths.split('/').filter((path) => path);
-  const pathItems = pathNames
-    .map((path, i) => ({
-      name: path,
-      path: pathNames.slice(0, i + 1).join('/'),
-    }));
+  const pathItems = pathNames.map((path, i) => ({
+    name: i === pathNames.length - 1 ? title : path,
+    path: pathNames.slice(0, i + 1).join('/'),
+  }));
 
   return (
     <section className={"breadcrumbs relative " + type}>
@@ -26,9 +27,8 @@ export const Breadcrumbs: FunctionComponent<Props> = ({ type }) => {
         <Image src="/bgs/bg10.jpg" alt="breadcrumb bg" fill />
         <div className="container text-center relative">
           <div className="page-info">
-            <h1>h1 Title here</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore</p>
+            <h1>{title}</h1>
+            <p>{desc}</p>
           </div>
           <ul className="page-breadcrumb">
             <li>
